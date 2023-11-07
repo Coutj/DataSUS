10d7b4d8b0e1738e9b8b6cf1954078b9e92661e6
Juan <coutinholima.j@gmail.com>
2023-11-07 20:33:15 -0300

    Using databricks operator parallelism to transform parquet

--- Airflow/dags/download_sus_data.py
+++ Airflow/dags/download_sus_data.py
@@ -3,10 +3,12 @@ from datetime import datetime
 from airflow.decorators import dag, task
 from airflow.providers.databricks.operators.databricks import DatabricksRunNowOperator
 from airflow.hooks.base import BaseHook
+from airflow.models import Variable
 
 
 @dag(
     dag_id='DataSus_Import',
+    # owner = 'Coutj',
     schedule='0 0 * * MON',
     start_date=datetime(2023, 11, 1),
     catchup=False,
@@ -32,19 +34,25 @@ def datasus_import():
         list_ufs = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 
                     'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 
                     'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
-
+        
         full_list_uf_years = list(product(list_ufs, years))
+        
+        # list_ufs = ['RJ']
+        # full_list_uf_years = [2019]
+
 
         adls_helper = ADLS(account_name, sas_token, 'datasus-data')
         dir_list = adls_helper.list_directory_contents('raw')
 
-        list_uf_year_in_adls = [(dir_path[6:8], int(dir_path[8:12])) for dir_path in dir_list]
-        # print(list_uf_year_in_adls)
+        list_uf_year_in_adls = [(dir_path[6:8], int(dir_path[8:12])) for dir_path in dir_list if '.parquet' in dir_path ]
+        print(list_uf_year_in_adls)
 
         files_to_download = list(set(full_list_uf_years) - set(list_uf_year_in_adls))
         files_to_download.sort()
 
         # print(files_to_download)
+
+        files_to_download = [('RJ', 2021)]
 
         return files_to_download
 
@@ -79,7 +87,7 @@ def datasus_import():
         sas_token = extra['sas_token']
 
         adls_helper = ADLS(account_name, sas_token, container_name='datasus-data')
-        print('Found files:', dir_path, list(Path(dir_path).glob('*.parquet')))
+        print('Found files:', dir_path, Path(dir_path).glob('*.parquet'))
 
         for dir in Path(dir_path).glob('*.parquet'):
             print(f'Uploading dir: {dir}')
@@ -98,13 +106,56 @@ def datasus_import():
         #     print(f'Del dir: {dir}')
         #     shutil.rmtree('/tmp/')
 
-    
+
+    @task(task_id='GetFilesToTransform')
+    def get_files_to_transform():
+        import json
+        from include.adls_helper import ADLS
+        from itertools import product
+        from pathlib import Path
+
+        # ADLS connection parameters
+        conn = BaseHook.get_connection("ADLS_DataSUS")
+        account_name = conn.login
+        extra = json.loads(conn.extra)
+        sas_token = extra['sas_token']
+
+        adls_helper = ADLS(account_name, sas_token, 'datasus-data')
+        dir_names_to_transform = adls_helper.list_directory_contents('raw')
+        dir_names_to_transform = [str(Path(dir).name) for dir in dir_names_to_transform]
+
+        dir_names_transformed = adls_helper.list_directory_contents('trusted')
+        dir_names_transformed = [str(Path(dir).name) for dir in dir_names_transformed]
+
+        dir_names_to_transform = list(set(dir_names_to_transform) - set(dir_names_transformed))
+        dir_names_to_transform.sort()
+        
+        print(dir_names_to_transform)
+        print(dir_names_transformed)
+
+        list_opr = ['DOAC2021.parquet', 'DOAC2020.parquet']
+        # dir_names_to_transform = list_opr
+        dir_names_to_transform = [ {'notebook_params': {'dir_name': dir}} for dir in dir_names_to_transform ]
+
+        return dir_names_to_transform
+
+
+
     files_to_download = get_files_list()
     download_files = extract(files_to_download)
     upload = upload_files(download_files)
     clean  = cleanup(download_files)
+    files_to_transform = get_files_to_transform()
 
-    files_to_download >> download_files >> upload >> clean
+    run_databricks = DatabricksRunNowOperator.partial(
+            task_id="Transform",
+            databricks_conn_id="databricks",
+            job_id=Variable.get('databricks_jobid'),
+            max_active_tis_per_dagrun=4).expand_kwargs(files_to_transform)
+
+    
+
+    files_to_download >> download_files >> upload >> clean >> files_to_transform >> run_databricks
 
 
-dag_instance = datasus_import()
+datasus_import()
